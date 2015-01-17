efd18e1f52f0170ba7eb0b756683404384fb4fe6
Manish Goregaokar <manishsmail@gmail.com>
2015-01-17 18:32:57 +0530

    Echo app, with goroutines!

--- assignment1/kvstore.go
+++ assignment1/kvstore.go
@@ -1,15 +1,24 @@
 package main
 
-import ("net")
+import (
+    "net"
+    "bufio"
+)
 
 const PORT = ":9000"
 
 func main () {
-    l, _ := net.Listen("tcp", PORT);
+    l, _ := net.Listen("tcp", PORT)
     for {
-        tx, _ := l.Accept();
+        conn, _ := l.Accept();
+        defer conn.Close()
+        go newSocket(conn)
+    }
+}
 
-        tx.Write([]byte("banana"));
-        tx.Close();
+func newSocket(conn net.Conn) {
+    scanner := bufio.NewScanner(conn)
+    for scanner.Scan() {
+        conn.Write([]byte("banana"+scanner.Text()+"\r\n"))
     }
 }
