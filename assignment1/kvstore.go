bebe2ad19e807df5e383ce74ef108ea0e7ec35d7
Manish Goregaokar <manishsmail@gmail.com>
2015-01-18 03:34:59 +0530

    gofmt, interference test

--- assignment1/kvstore.go
+++ assignment1/kvstore.go
@@ -1,299 +1,306 @@
 package main
 
 import (
-    "net"
-    "bufio"
-    "strings"
-    "strconv"
-    "errors"
-    "math/rand"
-    "fmt"
-    "os"
+	"bufio"
+	"errors"
+	"fmt"
+	"math/rand"
+	"net"
+	"os"
+	"strconv"
+	"strings"
 )
 
 const PORT = ":9000"
 
-
 // A value in the key-value store
 type Value struct {
-    val string
-    exptime int64
-    version int64
+	val     string
+	exptime int64
+	version int64
 }
 
 // A generic message to be passed to the backend
 type Message struct {
-    // The "acknowledgement" channel:
-    // will return acknowledgement from backend and
-    // any associated text
-    ack chan string
-    // The actual command (Set, Get, Getm, Cas, Delete)
-    // I would prefer to use an enum here, but that doesn't
-    // seem to be an option
-    data interface{}
+	// The "acknowledgement" channel:
+	// will return acknowledgement from backend and
+	// any associated text
+	ack chan string
+	// The actual command (Set, Get, Getm, Cas, Delete)
+	// I would prefer to use an enum here, but that doesn't
+	// seem to be an option
+	data interface{}
 }
 
 // Concrete message types
 
 // Set a key with given expiry time
 type Set struct {
-    key string
-    exptime int64
-    noreply bool
-    value string
+	key     string
+	exptime int64
+	noreply bool
+	value   string
 }
 
 // Get the value for a key
 type Get struct {
-    key string
+	key string
 }
 
 // Get the metadata for a key
 type Getm struct {
-    key string
+	key string
 }
 
 // Compare-and-swap a key's value iff the version matches
 type Cas struct {
-    key string
-    exptime int64
-    noreply bool
-    version int64
-    value string
+	key     string
+	exptime int64
+	noreply bool
+	version int64
+	value   string
 }
 
 // Delete a key
 type Delete struct {
-    key string
+	key string
 }
 
-func main () {
-    listener, err := net.Listen("tcp", PORT)
+func main() {
+	listener, err := net.Listen("tcp", PORT)
 
-    if err != nil {
-        fmt.Printf("Error setting up listener: %v", err.Error())
-        os.Exit(1)
-    }
+	if err != nil {
+		fmt.Printf("Error setting up listener: %v", err.Error())
+		os.Exit(1)
+	}
 
-    // This channel will queue messages from sockets
-    messages := make(chan Message)
+	// This channel will queue messages from sockets
+	messages := make(chan Message)
 
-    // Start backend goroutine
-    go backend(messages)
+	// Start backend goroutine
+	go backend(messages)
 
-    for {
-        conn, err := listener.Accept();
-        if err != nil {
-            fmt.Printf("Error starting connection: %v", err.Error())
-            continue
-        }
-        defer conn.Close()
-        // Spawn a socket goroutine
-        go newSocket(conn, messages)
-    }
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Printf("Error starting connection: %v", err.Error())
+			continue
+		}
+		defer conn.Close()
+		// Spawn a socket goroutine
+		go newSocket(conn, messages)
+	}
 }
 
 // Goroutine that handles interaction with a single connection
 // It can send structured messages via the Message channel
 func newSocket(conn net.Conn, cs chan Message) {
-    scanner := bufio.NewScanner(conn)
-    // scanner.Scan() automatically breaks at \r\n for us
-    for scanner.Scan() {
-        // Create acknowledgement channel
-        ack := make(chan string)
+	scanner := bufio.NewScanner(conn)
+	// scanner.Scan() automatically breaks at \r\n for us
+	for scanner.Scan() {
+		// Create acknowledgement channel
+		ack := make(chan string)
 
-        // Parse input
-        data, n, err := parse(scanner.Text())
-        if err != nil {
-            conn.Write([]byte(err.Error()))
-            continue
-        }
-        noreply := false
+		// Parse input
+		data, n, err := parse(scanner.Text())
+		if err != nil {
+			conn.Write([]byte(err.Error()))
+			continue
+		}
+		noreply := false
 
-        value := ""
-        // In case we are supposed to read a value of n bytes, read it
-        if n != -1 {
-            scanner.Scan()
-            value = scanner.Text()
-            if len(value) != n {
-                conn.Write([]byte("ERR_CMD_ERR\r\n"))
-                continue                
-            }
-        }
-        // Handle noreply
-        switch data.(type) {
-            case Set:
-                set := data.(Set)
-                if set.noreply {
-                    noreply = true
-                }
-                set.value = value
-                data = set
-            case Cas:
-                cas := data.(Cas)
-                if cas.noreply {
-                    noreply = true
-                }
-                cas.value = value
-                data = cas
-        }
+		value := ""
+		// In case we are supposed to read a value of n bytes, read it
+		if n != -1 {
+			scanner.Scan()
+			value = scanner.Text()
+			if len(value) != n {
+				conn.Write([]byte("ERR_CMD_ERR\r\n"))
+				continue
+			}
+		}
+		// Handle noreply
+		switch data.(type) {
+		case Set:
+			set := data.(Set)
+			if set.noreply {
+				noreply = true
+			}
+			set.value = value
+			data = set
+		case Cas:
+			cas := data.(Cas)
+			if cas.noreply {
+				noreply = true
+			}
+			cas.value = value
+			data = cas
+		}
 
-        message := Message {ack, data}
-        cs <- message
-        // Wait for acknowledgement
-        out :=  <- ack
-        conn.Write([]byte(out+"\r\n"))
-    }
+		message := Message{ack, data}
+		cs <- message
+		// Wait for acknowledgement
+		out := <-ack
+		conn.Write([]byte(out + "\r\n"))
+	}
 }
 
 // Goroutine that handles interaction with the k-v store
 // it will receive structured messages via the Message channel
 func backend(cs chan Message) {
-    // The actual keyvalue store
-    store := make(map[string]Value)
-    for message := range cs {
-        switch message.data.(type) {
-            case Set: {
-                data := message.data.(Set)
-                version := rand.Int63()
-                exptime := data.exptime
-                value := data.value
-                store[data.key] = Value {value, exptime, version}
-                message.ack <- "OK "+strconv.FormatInt(version, 10)
-            }
-            case Get: {
-                data := message.data.(Get)
-                value, ok := store[data.key]
-                if ok {
-                    val := value.val
-                    message.ack <- fmt.Sprintf("VALUE %v\r\n%v", len(val), val)
-                } else {
-                    message.ack <- "ERR_NOT_FOUND"
-                }
-            }
-            case Getm: {
-                data := message.data.(Getm)
-                value, ok := store[data.key]
-                if ok {
-                    val := value.val
-                    message.ack <- fmt.Sprintf("VALUE %v %v %v\r\n%v", value.version,
-                                               value.exptime, len(val), val)
-                } else {
-                    message.ack <- "ERR_NOT_FOUND"
-                }
-            }
-            case Delete: {
-                data := message.data.(Delete)
-                _, ok := store[data.key]
-                if ok {
-                    delete(store, data.key)
-                    message.ack <- "DELETED"
-                } else {
-                    message.ack <- "ERR_NOT_FOUND"
-                }
-            }
-            case Cas: {
-                data := message.data.(Cas)
-                val, ok := store[data.key]
-                if ok {
-                    if val.version == data.version {
-                        version := rand.Int63()
-                        exptime := data.exptime
-                        value := data.value
-                        store[data.key] = Value {value, exptime, version}
-                        message.ack <- "OK "+strconv.FormatInt(version, 10)
-                    } else {
-                        message.ack <- "ERR_VERSION"
-                    }
-                } else {
-                    message.ack <- "ERR_NOT_FOUND"
-                }                
-            }
-            default:
-                // this should never happen
-                message.ack <- "ERR_INTERNAL"
-        }
-    }
+	// The actual keyvalue store
+	store := make(map[string]Value)
+	for message := range cs {
+		switch message.data.(type) {
+		case Set:
+			{
+				data := message.data.(Set)
+				version := rand.Int63()
+				exptime := data.exptime
+				value := data.value
+				store[data.key] = Value{value, exptime, version}
+				message.ack <- "OK " + strconv.FormatInt(version, 10)
+			}
+		case Get:
+			{
+				data := message.data.(Get)
+				value, ok := store[data.key]
+				if ok {
+					val := value.val
+					message.ack <- fmt.Sprintf("VALUE %v\r\n%v", len(val), val)
+				} else {
+					message.ack <- "ERR_NOT_FOUND"
+				}
+			}
+		case Getm:
+			{
+				data := message.data.(Getm)
+				value, ok := store[data.key]
+				if ok {
+					val := value.val
+					message.ack <- fmt.Sprintf("VALUE %v %v %v\r\n%v", value.version,
+						value.exptime, len(val), val)
+				} else {
+					message.ack <- "ERR_NOT_FOUND"
+				}
+			}
+		case Delete:
+			{
+				data := message.data.(Delete)
+				_, ok := store[data.key]
+				if ok {
+					delete(store, data.key)
+					message.ack <- "DELETED"
+				} else {
+					message.ack <- "ERR_NOT_FOUND"
+				}
+			}
+		case Cas:
+			{
+				data := message.data.(Cas)
+				val, ok := store[data.key]
+				if ok {
+					if val.version == data.version {
+						version := rand.Int63()
+						exptime := data.exptime
+						value := data.value
+						store[data.key] = Value{value, exptime, version}
+						message.ack <- "OK " + strconv.FormatInt(version, 10)
+					} else {
+						message.ack <- "ERR_VERSION"
+					}
+				} else {
+					message.ack <- "ERR_NOT_FOUND"
+				}
+			}
+		default:
+			// this should never happen
+			message.ack <- "ERR_INTERNAL"
+		}
+	}
 }
 
 // Parse the input string and turn it into a concrete message type
 func parse(inp string) (interface{}, int, error) {
-    s := strings.Split(inp, " ")
-    err := errors.New("ERR_CMD_ERR\r\n")
-    n := -1
-    var ret interface {} = nil
-    switch s[0] {
-        case "set":
-            noreply := false
-            switch len(s) {
-                case 4:
-                case 5:
-                    if s[4] == "noreply" {
-                        noreply = true
-                    } else {
-                        return ret, n, err
-                    }
-                default: return ret, n, err
-            }
-            key := s[1]
-            exp, e := strconv.Atoi(s[2])
-            exptime := int64(exp)
-            if e != nil {
-                return ret, n, err
-            }
-            n, e := strconv.Atoi(s[3])
-            if e != nil {
-                return ret, n, err
-            }
-            ret = Set {key, exptime, noreply, ""}
-            return ret, n, nil
-        case "get":
-            if len(s) != 2 {
-                return ret, n, err
-            }
-            ret = Get{s[1]}
-            return ret, n, nil
-        case "getm":
-            if len(s) != 2 {
-                return ret, n, err
-            }
-            ret = Getm{s[1]}
-            return ret, n, nil
-        case "delete":
-            if len(s) != 2 {
-                return ret, n, err
-            }
-            ret = Delete{s[1]}
-            return ret, n, nil
-        case "cas":
-            noreply := false
-            switch len(s) {
-                case 5:
-                case 6:
-                    if s[5] == "noreply" {
-                        noreply = true
-                    } else {
-                        return ret, n, err
-                    }
-                default: return ret, n, err
-            }
-            key := s[1]
-            exp, e := strconv.Atoi(s[2])
-            exptime := int64(exp)
-            if e != nil {
-                return ret, n, err
-            }
-            ver, e := strconv.Atoi(s[3])
-            version := int64(ver)
-            if e != nil {
-                return ret, n, err
-            }
-            n, e := strconv.Atoi(s[4])
-            if e != nil {
-                return ret, n, err
-            }
-            ret = Cas {key, exptime, noreply, version, ""}
-            return ret, n, nil
-        default: return nil, n, err
-    }
+	s := strings.Split(inp, " ")
+	err := errors.New("ERR_CMD_ERR\r\n")
+	n := -1
+	var ret interface{} = nil
+	switch s[0] {
+	case "set":
+		noreply := false
+		switch len(s) {
+		case 4:
+		case 5:
+			if s[4] == "noreply" {
+				noreply = true
+			} else {
+				return ret, n, err
+			}
+		default:
+			return ret, n, err
+		}
+		key := s[1]
+		exp, e := strconv.Atoi(s[2])
+		exptime := int64(exp)
+		if e != nil {
+			return ret, n, err
+		}
+		n, e := strconv.Atoi(s[3])
+		if e != nil {
+			return ret, n, err
+		}
+		ret = Set{key, exptime, noreply, ""}
+		return ret, n, nil
+	case "get":
+		if len(s) != 2 {
+			return ret, n, err
+		}
+		ret = Get{s[1]}
+		return ret, n, nil
+	case "getm":
+		if len(s) != 2 {
+			return ret, n, err
+		}
+		ret = Getm{s[1]}
+		return ret, n, nil
+	case "delete":
+		if len(s) != 2 {
+			return ret, n, err
+		}
+		ret = Delete{s[1]}
+		return ret, n, nil
+	case "cas":
+		noreply := false
+		switch len(s) {
+		case 5:
+		case 6:
+			if s[5] == "noreply" {
+				noreply = true
+			} else {
+				return ret, n, err
+			}
+		default:
+			return ret, n, err
+		}
+		key := s[1]
+		exp, e := strconv.Atoi(s[2])
+		exptime := int64(exp)
+		if e != nil {
+			return ret, n, err
+		}
+		ver, e := strconv.Atoi(s[3])
+		version := int64(ver)
+		if e != nil {
+			return ret, n, err
+		}
+		n, e := strconv.Atoi(s[4])
+		if e != nil {
+			return ret, n, err
+		}
+		ret = Cas{key, exptime, noreply, version, ""}
+		return ret, n, nil
+	default:
+		return nil, n, err
+	}
 }
--- assignment1/kvstore_test.go
+++ assignment1/kvstore_test.go
@@ -1,162 +1,244 @@
 package main
 
 import (
-    "testing"
-    "fmt"
-    "strings"
-    "strconv"
+	"fmt"
+	"strconv"
+	"strings"
+	"testing"
 )
 
 // Tests for functioning of backend
-func TestBackend (t *testing.T) {
-    messages := make(chan Message)
-    go backend(messages)
-    ack := make(chan string)
+func TestBackend(t *testing.T) {
+	messages := make(chan Message)
+	go backend(messages)
+	ack := make(chan string)
 
-    // Check that the store is empty
-    message := Message{ack, Get{"banana"}}
-    messages<-message
-    resp := <- ack
-    expect(t, resp, "ERR_NOT_FOUND")
+	// Check that the store is empty
+	message := Message{ack, Get{"banana"}}
+	messages <- message
+	resp := <-ack
+	expect(t, resp, "ERR_NOT_FOUND")
 
-    message.data = Getm{"banana"}
-    messages<-message
-    resp = <- ack
-    expect(t, resp, "ERR_NOT_FOUND")
+	message.data = Getm{"banana"}
+	messages <- message
+	resp = <-ack
+	expect(t, resp, "ERR_NOT_FOUND")
 
-    message.data = Delete{"banana"}
-    messages<-message
-    resp = <- ack
-    expect(t, resp, "ERR_NOT_FOUND")
+	message.data = Delete{"banana"}
+	messages <- message
+	resp = <-ack
+	expect(t, resp, "ERR_NOT_FOUND")
 
-    // Add a single k-v pair to the store
-    message.data = Set{key: "banana", exptime: 42, noreply: false, value: "potato"}
-    messages<-message
-    resp = <- ack
-    arr := strings.Split(resp, " ")
-    expect(t, arr[0], "OK")
-    ver, err := strconv.Atoi(arr[1])
-    if err != nil {
-        t.Error("Non-numeric version found")
-    }
-    version := int64(ver)
+	// Add a single k-v pair to the store
+	message.data = Set{key: "banana", exptime: 42, noreply: false, value: "potato"}
+	messages <- message
+	resp = <-ack
+	arr := strings.Split(resp, " ")
+	expect(t, arr[0], "OK")
+	ver, err := strconv.Atoi(arr[1])
+	if err != nil {
+		t.Error("Non-numeric version found")
+	}
+	version := int64(ver)
 
-    // Test that the k-v pair exists and Get/Getm are working properly
-    message.data = Get{"banana"}
-    messages<-message
-    resp = <- ack
-    expect(t, resp, "VALUE 6\r\npotato")
+	// Test that the k-v pair exists and Get/Getm are working properly
+	message.data = Get{"banana"}
+	messages <- message
+	resp = <-ack
+	expect(t, resp, "VALUE 6\r\npotato")
 
-    message.data = Getm{"banana"}
-    messages<-message
-    resp = <- ack
-    expect(t, resp, fmt.Sprintf("VALUE %v 42 6\r\npotato", version))
+	message.data = Getm{"banana"}
+	messages <- message
+	resp = <-ack
+	expect(t, resp, fmt.Sprintf("VALUE %v 42 6\r\npotato", version))
 
-    // Add another k-v pair to test for clashes
-    message.data = Set{key: "watermelon", exptime: 100, noreply: false, value: "tomato"}
-    messages<-message
-    resp = <- ack
+	// Add another k-v pair to test for clashes
+	message.data = Set{key: "watermelon", exptime: 100, noreply: false, value: "tomato"}
+	messages <- message
+	resp = <-ack
 
-    // Check that they still work
-    message.data = Get{"banana"}
-    messages<-message
-    resp = <- ack
-    expect(t, resp, "VALUE 6\r\npotato")
+	// Check that they still work
+	message.data = Get{"banana"}
+	messages <- message
+	resp = <-ack
+	expect(t, resp, "VALUE 6\r\npotato")
 
-    message.data = Getm{"banana"}
-    messages<-message
-    resp = <- ack
-    expect(t, resp, fmt.Sprintf("VALUE %v 42 6\r\npotato", version))
+	message.data = Getm{"banana"}
+	messages <- message
+	resp = <-ack
+	expect(t, resp, fmt.Sprintf("VALUE %v 42 6\r\npotato", version))
 
-    // CAS with bad version
-    message.data = Cas{key: "banana", exptime: 42, noreply: false, value: "tomato", version: version + 100}
-    messages<-message
-    resp = <- ack
-    expect(t, resp, "ERR_VERSION")
+	// CAS with bad version
+	message.data = Cas{key: "banana", exptime: 42, noreply: false, value: "tomato", version: version + 100}
+	messages <- message
+	resp = <-ack
+	expect(t, resp, "ERR_VERSION")
 
-    // CAS with bad key
-    message.data = Cas{key: "pineapple", exptime: 42, noreply: false, value: "tomato", version: version + 100}
-    messages<-message
-    resp = <- ack
-    expect(t, resp, "ERR_NOT_FOUND")
+	// CAS with bad key
+	message.data = Cas{key: "pineapple", exptime: 42, noreply: false, value: "tomato", version: version + 100}
+	messages <- message
+	resp = <-ack
+	expect(t, resp, "ERR_NOT_FOUND")
 
-    // CAS with good version
-    message.data = Cas{key: "banana", exptime: 100, noreply: false, value: "kiwi", version: version}
-    messages<-message
-    resp = <- ack
-    arr = strings.Split(resp, " ")
-    expect(t, arr[0], "OK")
-    ver, err = strconv.Atoi(arr[1])
-    if err != nil {
-        t.Error("Non-numeric version found")
-    }
-    version = int64(ver)
+	// CAS with good version
+	message.data = Cas{key: "banana", exptime: 100, noreply: false, value: "kiwi", version: version}
+	messages <- message
+	resp = <-ack
+	arr = strings.Split(resp, " ")
+	expect(t, arr[0], "OK")
+	ver, err = strconv.Atoi(arr[1])
+	if err != nil {
+		t.Error("Non-numeric version found")
+	}
+	version = int64(ver)
 
-    // Check that it was updated
-    message.data = Get{"banana"}
-    messages<-message
-    resp = <- ack
-    expect(t, resp, "VALUE 4\r\nkiwi")
+	// Check that it was updated
+	message.data = Get{"banana"}
+	messages <- message
+	resp = <-ack
+	expect(t, resp, "VALUE 4\r\nkiwi")
 
-    message.data = Getm{"banana"}
-    messages<-message
-    resp = <- ack
-    expect(t, resp, fmt.Sprintf("VALUE %v 100 4\r\nkiwi", version))
+	message.data = Getm{"banana"}
+	messages <- message
+	resp = <-ack
+	expect(t, resp, fmt.Sprintf("VALUE %v 100 4\r\nkiwi", version))
 
-    // Remove watermelon
-    message.data = Delete{"watermelon"}
-    messages<-message
-    resp = <- ack
-    expect(t, resp, "DELETED")
+	// Remove watermelon
+	message.data = Delete{"watermelon"}
+	messages <- message
+	resp = <-ack
+	expect(t, resp, "DELETED")
 
-    // `watermelon` should be gone
-    message.data = Get{"watermelon"}
-    messages<-message
-    resp = <- ack
-    expect(t, resp, "ERR_NOT_FOUND")
+	// `watermelon` should be gone
+	message.data = Get{"watermelon"}
+	messages <- message
+	resp = <-ack
+	expect(t, resp, "ERR_NOT_FOUND")
 
-    message.data = Getm{"watermelon"}
-    messages<-message
-    resp = <- ack
-    expect(t, resp, "ERR_NOT_FOUND")
+	message.data = Getm{"watermelon"}
+	messages <- message
+	resp = <-ack
+	expect(t, resp, "ERR_NOT_FOUND")
 
-    // `banana` should still exist
-    message.data = Get{"banana"}
-    messages<-message
-    resp = <- ack
-    expect(t, resp, "VALUE 4\r\nkiwi")
+	// `banana` should still exist
+	message.data = Get{"banana"}
+	messages <- message
+	resp = <-ack
+	expect(t, resp, "VALUE 4\r\nkiwi")
 
-    message.data = Getm{"banana"}
-    messages<-message
-    resp = <- ack
-    expect(t, resp, fmt.Sprintf("VALUE %v 100 4\r\nkiwi", version))
+	message.data = Getm{"banana"}
+	messages <- message
+	resp = <-ack
+	expect(t, resp, fmt.Sprintf("VALUE %v 100 4\r\nkiwi", version))
 
-    // Remove `banana`
-    message.data = Delete{"banana"}
-    messages<-message
-    resp = <- ack
-    expect(t, resp, "DELETED")
+	// Remove `banana`
+	message.data = Delete{"banana"}
+	messages <- message
+	resp = <-ack
+	expect(t, resp, "DELETED")
 
-    // `banana` should now be gone
-    message.data = Get{"banana"}
-    messages<-message
-    resp = <- ack
-    expect(t, resp, "ERR_NOT_FOUND")
+	// `banana` should now be gone
+	message.data = Get{"banana"}
+	messages <- message
+	resp = <-ack
+	expect(t, resp, "ERR_NOT_FOUND")
 
-    message.data = Getm{"banana"}
-    messages<-message
-    resp = <- ack
-    expect(t, resp, "ERR_NOT_FOUND")
+	message.data = Getm{"banana"}
+	messages <- message
+	resp = <-ack
+	expect(t, resp, "ERR_NOT_FOUND")
 
-    // CAS with deleted key
-    message.data = Cas{key: "banana", exptime: 42, noreply: false, value: "tomato", version: version + 100}
-    messages<-message
-    resp = <- ack
-    expect(t, resp, "ERR_NOT_FOUND")
+	// CAS with deleted key
+	message.data = Cas{key: "banana", exptime: 42, noreply: false, value: "tomato", version: version + 100}
+	messages <- message
+	resp = <-ack
+	expect(t, resp, "ERR_NOT_FOUND")
+}
+
+func TestBackendConcurrent(t *testing.T) {
+
+	messages := make(chan Message)
+	done := make(chan bool)
+	go backend(messages)
+	// Add some background noise
+	for i := 0; i < 5; i++ {
+		go interference(t, messages, done, "i"+strconv.Itoa(i),
+			"tomato"+strconv.Itoa(i), int64(100+i))
+	}
+
+	for i := 0; i < 5; i++ {
+		<-done
+	}
+}
+
+// An independent test to be spawned (to trigger interference)
+// Just sets, swaps, and deletes a single key
+func interference(t *testing.T, messages chan Message, done chan bool, key string, value string, exptime int64) {
+	for i := 0; i < 10; i++ {
+		ack := make(chan string)
+		message := Message{ack, Set{key: key, exptime: exptime, noreply: false, value: value}}
+		messages <- message
+		resp := <-ack
+		arr := strings.Split(resp, " ")
+		expect(t, arr[0], "OK")
+		ver, err := strconv.Atoi(arr[1])
+		if err != nil {
+			t.Error("Non-numeric version found")
+		}
+		version := int64(ver)
+
+		message.data = Get{key}
+		messages <- message
+		resp = <-ack
+		expect(t, resp, fmt.Sprintf("VALUE %v\r\n%v", len(value), value))
+
+		message.data = Getm{key}
+		messages <- message
+		resp = <-ack
+		expect(t, resp, fmt.Sprintf("VALUE %v %v %v\r\n%v", version, exptime, len(value), value))
+
+		message.data = Cas{key: key, exptime: exptime, noreply: false, value: value, version: version}
+		messages <- message
+		resp = <-ack
+		arr = strings.Split(resp, " ")
+		expect(t, arr[0], "OK")
+		ver, err = strconv.Atoi(arr[1])
+		if err != nil {
+			t.Error("Non-numeric version found")
+		}
+		version = int64(ver)
+
+		message.data = Get{key}
+		messages <- message
+		resp = <-ack
+		expect(t, resp, fmt.Sprintf("VALUE %v\r\n%v", len(value), value))
+
+		message.data = Getm{key}
+		messages <- message
+		resp = <-ack
+		expect(t, resp, fmt.Sprintf("VALUE %v %v %v\r\n%v", version, exptime, len(value), value))
+
+		message.data = Delete{key}
+		messages <- message
+		resp = <-ack
+		expect(t, resp, "DELETED")
+
+		// `banana` should now be gone
+		message.data = Get{key}
+		messages <- message
+		resp = <-ack
+		expect(t, resp, "ERR_NOT_FOUND")
+
+		message.data = Getm{key}
+		messages <- message
+		resp = <-ack
+		expect(t, resp, "ERR_NOT_FOUND")
+	}
+	done <- true
 }
 
 func expect(t *testing.T, a string, b string) {
-    if a != b {
-        t.Error(fmt.Sprintf("Expected %v, found %v", b, a))
-    }
+	if a != b {
+		t.Error(fmt.Sprintf("Expected %v, found %v", b, a))
+	}
 }
