3150ee568641d0196ab8bc7c034dadb8b4d2aff7
Manish Goregaokar <manishsmail@gmail.com>
2015-01-17 23:50:50 +0530

    fix noreply

--- assignment1/kvstore.go
+++ assignment1/kvstore.go
@@ -14,7 +14,7 @@ import (
 const PORT = ":9000"
 
 func main () {
-    l, err := net.Listen("tcp", PORT)
+    listener, err := net.Listen("tcp", PORT)
 
     if err != nil {
         fmt.Printf("Error setting up listener: %v", err.Error())
@@ -24,15 +24,17 @@ func main () {
     // This channel will queue messages from sockets
     messages := make(chan Message)
 
+    // Start backend goroutine
     go backend(messages)
 
     for {
-        conn, err := l.Accept();
+        conn, err := listener.Accept();
         if err != nil {
             fmt.Printf("Error starting connection: %v", err.Error())
             continue
         }
         defer conn.Close()
+        // Spawn a socket goroutine
         go newSocket(conn, messages)
     }
 }
@@ -41,6 +43,7 @@ func main () {
 // It can send structured messages via the Message channel
 func newSocket(conn net.Conn, cs chan Message) {
     scanner := bufio.NewScanner(conn)
+    // scanner.Scan() automatically breaks at \r\n for us
     for scanner.Scan() {
         // Create acknowledgement channel
         ack := make(chan string)
@@ -52,6 +55,8 @@ func newSocket(conn net.Conn, cs chan Message) {
             continue
         }
         message := Message {ack, "", data}
+
+        // In case we are supposed to read a value of n bytes, read it
         if n != -1 {
             scanner.Scan()
             message.value = scanner.Text()
@@ -63,6 +68,18 @@ func newSocket(conn net.Conn, cs chan Message) {
         cs <- message
         // Wait for acknowledgement
         out :=  <- ack
+
+        // Handle noreply
+        switch message.data.(type) {
+            case Set:
+                if message.data.(Set).noreply {
+                    continue
+                }
+            case Cas:
+                if message.data.(Cas).noreply {
+                    continue
+                }
+        }
         conn.Write([]byte(out+"\r\n"))
     }
 }
@@ -70,13 +87,13 @@ func newSocket(conn net.Conn, cs chan Message) {
 // Goroutine that handles interaction with the k-v store
 // it will receive structured messages via the Message channel
 func backend(cs chan Message) {
+    // The actual keyvalue store
     store := make(map[string]Value)
     for {
         message :=  <- cs
         switch message.data.(type) {
             case Set: {
                 data := message.data.(Set)
-                // might be nice to use a UUID here
                 version := rand.Int63()
                 value := message.value
                 exptime := data.exptime
@@ -98,7 +115,8 @@ func backend(cs chan Message) {
                 value, ok := store[data.key]
                 if ok {
                     val := value.val
-                    message.ack <- fmt.Sprintf("VALUE %v %v %v\r\n%v", value.version, value.exptime, len(val), val)
+                    message.ack <- fmt.Sprintf("VALUE %v %v %v\r\n%v", value.version,
+                                               value.exptime, len(val), val)
                 } else {
                     message.ack <- "ERR_NOT_FOUND"
                 }
@@ -129,7 +147,6 @@ func backend(cs chan Message) {
                 } else {
                     message.ack <- "ERR_NOT_FOUND"
                 }                
-                // might be nice to use a UUID here
             }
             default:
                 message.ack <- "ERR_INTERNAL"
@@ -137,18 +154,24 @@ func backend(cs chan Message) {
     }
 }
 
+// A value in the key-value store
 type Value struct {
     val string
     exptime int64
     version int64
 }
 
+// A generic message to be passed to the backend
 type Message struct {
     // The "acknowledgement" channel:
     // will return acknowledgement from backend and
     // any associated text
     ack chan string
+    // A vlaue, if any
     value string
+    // The actual command (Set, Get, Getm, Cas, Delete)
+    // I would prefer to use an enum here, but that doesn't
+    // seem to be an option
     data interface{}
 }
 
