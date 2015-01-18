e6c96f913024af38a2d875c43970da9648096ac6
Manish Goregaokar <manishsmail@gmail.com>
2015-01-18 06:00:11 +0530

    TCP test

--- assignment1/kvstore.go
+++ assignment1/kvstore.go
@@ -141,9 +141,9 @@ func newSocket(conn net.Conn, cs chan Message) {
 		cs <- message
 		// Wait for acknowledgement
 		out := <-ack
-        if !noreply {
-            conn.Write([]byte(out + "\r\n"))            
-        }
+		if !noreply {
+			conn.Write([]byte(out + "\r\n"))
+		}
 	}
 }
 
--- assignment1/kvstore_test.go
+++ assignment1/kvstore_test.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"net"
 	"strconv"
 	"strings"
 	"testing"
+	"time"
 )
 
 // Serial tests for functioning of backend
@@ -158,25 +161,25 @@ func TestBackend(t *testing.T) {
 // This tests the backend's ability to hand lots
 // of clients at once, with some racing attempts to CAS
 func TestBackendConcurrent(t *testing.T) {
-    done := make (chan bool)
+	done := make(chan bool)
 	messages := make(chan Message)
 	go backend(messages)
 	// Add some background noise. This shouldn't race, but it tests
-    // our ability to buffer and handle load
+	// our ability to buffer and handle load
 	for i := 0; i < 5; i++ {
 		go interference(t, messages, done, "i"+strconv.Itoa(i),
 			"tomato"+strconv.Itoa(i), int64(100+i))
 	}
 
-    // Each one of these is an concurrency test
-    // which will try to race with itself
-    go concurrent(t, messages, done, "prefix1")
-    go concurrent(t, messages, done, "prefix2")
-    go concurrent(t, messages, done, "prefix3")
+	// Each one of these is an concurrency test
+	// which will try to race with itself
+	go concurrent(t, messages, done, "prefix1")
+	go concurrent(t, messages, done, "prefix2")
+	go concurrent(t, messages, done, "prefix3")
 
-	for i := 0; i< 9; i++ {
-        <- done
-    }
+	for i := 0; i < 9; i++ {
+		<-done
+	}
 }
 
 // An independent goroutine to be spawned (to trigger interference)
@@ -242,12 +245,12 @@ func interference(t *testing.T, messages chan Message, done chan bool, key strin
 		resp = <-ack
 		expect(t, resp, "ERR_NOT_FOUND")
 	}
-    done <- true
+	done <- true
 }
 
 // Sets a key with the given prefix, then spawns two threads competing to CAS it
 func concurrent(t *testing.T, messages chan Message, done chan bool, prefix string) {
-	key := prefix+"-concurrent"
+	key := prefix + "-concurrent"
 	exptime := int64(100)
 	value := "set"
 	ack := make(chan string)
@@ -261,32 +264,32 @@ func concurrent(t *testing.T, messages chan Message, done chan bool, prefix stri
 		t.Error("Non-numeric version found")
 	}
 	version := int64(ver)
-    // The first one almost always wins, but inserting various fudge factors between the two
-    // leads to varying and interesting behavior
-    go concurrentInner(t, messages, done, key, version, "setby1", "setby2", "First")
+	// The first one almost always wins, but inserting various fudge factors between the two
+	// leads to varying and interesting behavior
+	go concurrentInner(t, messages, done, key, version, "setby1", "setby2", "First")
 	go concurrentInner(t, messages, done, key, version, "setby2", "setby1", "Second")
 }
 
 // CAS the given key, report if successful, and check validity
 func concurrentInner(t *testing.T, messages chan Message, done chan bool, key string,
-                     version int64, value string, othervalue string, name string) {
-    ack := make(chan string)
-    exptime := int64(200)
+	version int64, value string, othervalue string, name string) {
+	ack := make(chan string)
+	exptime := int64(200)
 	message := Message{ack, Cas{key: key, exptime: exptime, noreply: false, value: value, version: version}}
 	messages <- message
 
 	resp := <-ack
 	arr := strings.Split(resp, " ")
-    if arr[0] == "OK" {
-        t.Logf("%v concurrent goroutine won", name)
-    } else {
-        value = othervalue
-    }
-    message.data = Get{key}
-    messages <- message
-    resp = <-ack
-    expect(t, resp, fmt.Sprintf("VALUE %v\r\n%v", len(value), value))
-    done <- true
+	if arr[0] == "OK" {
+		t.Logf("%v concurrent goroutine won", name)
+	} else {
+		value = othervalue
+	}
+	message.data = Get{key}
+	messages <- message
+	resp = <-ack
+	expect(t, resp, fmt.Sprintf("VALUE %v\r\n%v", len(value), value))
+	done <- true
 }
 
 // Useful testing function
@@ -294,4 +297,52 @@ func expect(t *testing.T, a string, b string) {
 	if a != b {
 		t.Error(fmt.Sprintf("Expected %v, found %v", b, a))
 	}
+}
+
+func TestTCP(t *testing.T) {
+	go main()
+	time.Sleep(time.Microsecond * time.Duration(100))
+
+	done := make(chan bool)
+	go singleTCP(t, done, "hi", "bye")
+	// Everything will just exit here, no need to close goroutines explicitly
+	for i := 0; i < 1; i++ {
+		<-done
+	}
+}
+
+func singleTCP(t *testing.T, done chan bool, name string, value string) {
+	exptime := 300
+	conn, err := net.Dial("tcp", fmt.Sprintf("localhost%v", PORT))
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	fmt.Fprintf(conn, "get %v\r\n", name)
+	scanner := bufio.NewScanner(conn)
+	scanner.Scan()
+
+	expect(t, "ERR_NOT_FOUND", scanner.Text())
+	fmt.Fprintf(conn, "set %v %v %v\r\n%v\r\n", name, exptime, len(value), value)
+	scanner.Scan()
+	resp := scanner.Text()
+	arr := strings.Split(resp, " ")
+	expect(t, "OK", arr[0])
+	ver, err := strconv.Atoi(arr[1])
+	if err != nil {
+		t.Error("Non-numeric version found")
+	}
+	version := int64(ver)
+
+	fmt.Fprintf(conn, "get %v\r\n", name)
+	scanner.Scan()
+	expect(t, fmt.Sprintf("VALUE %v", len(value)), scanner.Text())
+	scanner.Scan()
+	expect(t, value, scanner.Text())
+	fmt.Fprintf(conn, "getm %v\r\n", name)
+	scanner.Scan()
+	expect(t, fmt.Sprintf("VALUE %v %v %v", version, exptime, len(value)), scanner.Text())
+	scanner.Scan()
+	expect(t, value, scanner.Text())
+	done <- true
 }
